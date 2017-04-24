b489f77c280fb2b16d405a265fa412a1fe6bbb34
Liviu <stefan.liviu.daniel@gmail.com>
2017-04-24 16:01:37 +0200

    Added the resize option

--- create_data_lmdb.sh
+++ create_data_lmdb.sh
@@ -1,19 +1,10 @@
-# -------------------------------------------------------------------
-# Create the LMDB for the data instances
-# Both train and validation lmdbs can be created using this 
-# The file is adapted from BVLC Caffe, and requires Caffe tools
-# Author: Sukrit Shankar 
-# -------------------------------------------------------------------
 
-# +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 # Please set the appropriate paths
-EXAMPLE=/home/sukrit/Desktop/caffe_project/lmdbs       			# Path where the output LMDB is stored
-DATA=/home/sukrit/Desktop/caffe_project/datasets       			# Path where the data.txt file is present 
-TOOLS=/home/sukrit/Desktop/caffe-master/build/tools    			# Caffe dependency to access the convert_imageset utility 
-DATA_ROOT=/home/sukrit/Desktop/datasets/images/     			# Path prefix for each entry in data.txt
-# +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+EXAMPLE=/caffe_project/lmdbs       			# Path where the output LMDB is stored
+DATA=/caffe_project/datasets       			# Path where the data.txt file is present 
+TOOLS=/caffe-master/build/tools    			# Caffe dependency to access the convert_imageset utility 
+DATA_ROOT=/datasets/images/     			  # Path prefix for each entry in data.txt
 
-# ----------------------------
 # Checks for DATA_ROOT Path
 if [ ! -d "$DATA_ROOT" ]; then
   echo "Error: DATA_ROOT is not a path to a directory: $DATA_ROOT"
@@ -21,10 +12,22 @@ if [ ! -d "$DATA_ROOT" ]; then
   exit 1
 fi
 
+# Set the resize options
+RESIZE=true
+if $RESIZE; then
+  RESIZE_HEIGHT=224
+  RESIZE_WIDTH=224
+else
+  RESIZE_HEIGHT=0
+  RESIZE_WIDTH=0
+fi
+
 # ------------------------------
 # Creating LMDB
  echo "Creating data lmdb..."
  GLOG_logtostderr=1 $TOOLS/convert_imageset \
+    --resize_height=$RESIZE_HEIGHT \
+    --resize_width=$RESIZE_WIDTH \
     $DATA_ROOT \
     $DATA/data.txt \
     $EXAMPLE/data_lmdb
